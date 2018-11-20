1b0529587317d7ff590f870ff080f6f28e86c3b5
Hiroki-Yumigeta <s.1526122TM@s.chibakoudai.jp>
2018-11-20 15:47:33 +0900

    add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# servo
-pi-tobitaのservoフォルダ バックアップ
+# RS304mdライター
+Futaba RS304MDで使えるサーボライター
+RS306MD RS303MRでも使用可能
--- src/MotionSet/nkf.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/pi/Program/servo/parameter/
-nkf -Lu --overwrite *.csv
-cd ./Ahead/
-nkf -Lu --overwrite *.csv
-
